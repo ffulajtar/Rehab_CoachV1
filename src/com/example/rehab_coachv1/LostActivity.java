6f9d1c7d33b6812d99935c8496ae96e70a77e454
ffulajtar <fulajtar.2@buckeyemail.osu.edu>
2014-03-04 23:00:09 -0500

    Ok now im done for the night

--- src/com/example/rehab_coachv1/LostActivity.java
+++ src/com/example/rehab_coachv1/LostActivity.java
@@ -2,7 +2,9 @@ package com.example.rehab_coachv1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class LostActivity extends Activity {
 
@@ -35,5 +37,47 @@ public class LostActivity extends Activity {
 		}
 		return super.onCreateOptionsMenu(menu);
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+
+	        case R.id.home_screen:
+	            openHome();
+	            return true;
+	        case R.id.profile_screen:
+	            openProfile();
+	            return true;
+	        case R.id.help_screen:
+	            openHelp();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	    
+		public void openHome() {
+			Intent remind = new Intent (this, HomeActivity.class);
+			remind.putExtra("theme", theme);
+			startActivity(remind);
+			
+		}
+
+		public void openProfile() {
+			Intent remind = new Intent (this, ProfileActivity.class);
+			remind.putExtra("theme", theme);
+			startActivity(remind);
+			
+		}
+
+		public void openHelp() {
+			Intent remind = new Intent (this, HelpActivity.class);
+			remind.putExtra("theme", theme);
+			startActivity(remind);	
+		}
+
 
 }
+
+	
